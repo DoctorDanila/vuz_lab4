549efe12128587bcb8e27a5227034539ac65761f
Strange_man <50800526+DoctorDanila@users.noreply.github.com>
2025-01-10 09:02:01 +0500

    first commit

---
+++ lab4/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Задание 1: Проверка удаления дубликатов
+            Console.WriteLine("Задание 1: Удаление дубликатов из списка");
+            List<int> numbers = new List<int> { 1, 2, 2, 3, 4, 4, 5 };
+            var uniqueNumbers = WorkClass.RemoveDuplicates(numbers);
+            Console.WriteLine($"Уникальные числа: {string.Join(", ", uniqueNumbers)}\n");
+
+            // Задание 2: Проверка смены соседей в LinkedList
+            Console.WriteLine("Задание 2: Смена соседей в LinkedList");
+            LinkedList<int> linkedList = new LinkedList<int>(new[] { 1, 2, 2, 3, 4 });
+            WorkClass.SwapNeighborsIfNotEqual(linkedList);
+            Console.WriteLine($"Результат LinkedList: {string.Join(", ", linkedList)}\n");
+
+            // Задание 3: Проверка анализа языков
+            Console.WriteLine("Задание 3: Анализ языков работников");
+            var employeeLanguages = new Dictionary<string, HashSet<string>>
+            {
+                { "Иванов", new HashSet<string> { "Русский", "Английский" } },
+                { "Петров", new HashSet<string> { "Английский", "Немецкий" } },
+                { "Сидоров", new HashSet<string> { "Французский" } }
+            };
+            WorkClass.AnalyzeLanguages(employeeLanguages);
+            Console.WriteLine();
+
+            // Задание 4: Проверка получения начальных букв слов
+            Console.WriteLine("Задание 4: Начальные буквы слов в тексте");
+            string text = "Привет мир! Это тестовый текст.";
+            var startingLetters = WorkClass.GetStartingLetters(text);
+            Console.WriteLine($"Начальные буквы: {string.Join(", ", startingLetters)}\n");
+
+            // Задание 5: Проверка нахождения лучших учеников
+            Console.WriteLine("Задание 5: Нахождение лучших учеников школы №50");
+            var students = new List<WorkClass.Student>
+            {
+                new WorkClass.Student { LastName = "Иванов", FirstName = "Сергей", SchoolNumber = 50, Score = 87 },
+                new WorkClass.Student { LastName = "Сергеев", FirstName = "Иван", SchoolNumber = 50, Score = 92 },
+                new WorkClass.Student { LastName = "Кузнецов", FirstName = "Алексей", SchoolNumber = 50, Score = 92 },
+                new WorkClass.Student { LastName = "Петров", FirstName = "Дмитрий", SchoolNumber = 51, Score = 85 }
+            };
+
+            WorkClass.FindTopStudents(students);
+
+            // Пример сериализации и десериализации студентов
+            //string filePath = "students.xml";
+
+            //Console.WriteLine("\nСериализация студентов в XML...");
+            //WorkClass.SerializeStudentsToXml(filePath, students);
+
+            //Console.WriteLine("Десериализация студентов из XML...");
+            //var deserializedStudents = WorkClass.DeserializeStudentsFromXml(filePath);
+
+            //Console.WriteLine("Десериализованные студенты:");
+            //foreach (var student in deserializedStudents)
+            //{
+            //    Console.WriteLine($"{student.LastName} {student.FirstName} - Школа: {student.SchoolNumber}, Балл: {student.Score}");
+            //}
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ lab4/WorkClass.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace lab4
+{
+    internal class WorkClass
+    {
+        // Задание 1: Оставить в списке только первые вхождения одинаковых элементов.
+        public static List<T> RemoveDuplicates<T>(List<T> list)
+        {
+            return list.Distinct().ToList();
+        }
+
+        // Задание 2: Если у элемента со значением E "соседи" не равны, поменять их местами.
+        public static void SwapNeighborsIfNotEqual(LinkedList<int> list)
+        {
+            var current = list.First;
+            while (current != null && current.Next != null)
+            {
+                if (current.Value != current.Next.Value)
+                {
+                    var next = current.Next;
+                    int temp = current.Value;
+                    current.Value = next.Value;
+                    next.Value = temp;
+                }
+                current = current.Next;
+            }
+        }
+
+        // Задание 3: Определить, какие языки знает каждый работник, хотя бы один и никто.
+        public static void AnalyzeLanguages(Dictionary<string, HashSet<string>> employeeLanguages)
+        {
+            var allLanguages = new HashSet<string>();
+            foreach (var languages in employeeLanguages.Values)
+            {
+                allLanguages.UnionWith(languages);
+            }
+
+            foreach (var language in allLanguages)
+            {
+                var knowsLanguage = employeeLanguages.Where(e => e.Value.Contains(language)).Select(e => e.Key).ToList();
+                Console.WriteLine($"Язык: {language}, Знают: {string.Join(", ", knowsLanguage)}");
+            }
+
+            var noOneKnows = allLanguages.Where(lang => employeeLanguages.All(e => !e.Value.Contains(lang)));
+            Console.WriteLine($"Языки, которые никто не знает: {string.Join(", ", noOneKnows)}");
+        }
+
+        // Задание 4: С каких букв начинаются слова в тексте.
+        public static HashSet<char> GetStartingLetters(string text)
+        {
+            var words = text.Split(new[] { ' ', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+            return new HashSet<char>(words.Select(word => word[0]));
+        }
+
+        // Задание 5: Определение лучших учеников школы № 50.
+        [Serializable]
+        public class Student
+        {
+            public string LastName { get; set; }
+            public string FirstName { get; set; }
+            public int SchoolNumber { get; set; }
+            public int Score { get; set; }
+
+            // Конструктор по умолчанию необходим для сериализации
+            public Student() { }
+        }
+
+        public static void FindTopStudents(List<Student> students)
+        {
+            var school50Students = students.Where(s => s.SchoolNumber == 50).ToList();
+
+            if (!school50Students.Any()) return;
+
+            var maxScore = school50Students.Max(s => s.Score);
+            var topStudents = school50Students.Where(s => s.Score == maxScore).ToList();
+
+            if (topStudents.Count > 2)
+            {
+                Console.WriteLine(topStudents.Count);
+            }
+            else if (topStudents.Count == 1)
+            {
+                Console.WriteLine($"{topStudents[0].LastName} {topStudents[0].FirstName}");
+            }
+            else
+            {
+                foreach (var student in topStudents)
+                {
+                    Console.WriteLine($"{student.LastName} {student.FirstName}");
+                }
+            }
+        }
+
+        // Метод для заполнения списка студентов и сериализации в XML
+        public static void SerializeStudentsToXml(string filePath, List<Student> students)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(List<Student>));
+
+            using (StreamWriter writer = new StreamWriter(filePath))
+            {
+                serializer.Serialize(writer, students);
+            }
+        }
+
+        // Метод для десериализации студентов из XML
+        public static List<Student> DeserializeStudentsFromXml(string filePath)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(List<Student>));
+
+            using (StreamReader reader = new StreamReader(filePath))
+            {
+                return (List<Student>)serializer.Deserialize(reader);
+            }
+        }
+    }
+}
